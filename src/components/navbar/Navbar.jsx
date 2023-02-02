de40ec54cd3f62a71f983c4ad8fd5a18ccfd12f9
Steven Sidhu <steve.sidhu@hotmail.com>
2023-02-02 01:03:14 -0800

    setup BEM Naming Convention

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import { RiMenu3Line, RiCloseLin } from "react-icons/ri";
 import "./navbar.css";
 
+// BEM -> Block Element Modifier Naming Convention
+
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar__links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
