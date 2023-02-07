fa39e8e81af8eaaf0ddec2e8c6d9f383c9d5cc56
Steven Sidhu <steve.sidhu@hotmail.com>
2023-02-06 23:12:59 -0800

    Fix - Placed div in correct spot. Fixed.

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -4,11 +4,13 @@ import "./feature.css";
 // passing title and text as props
 function Feature({ title, text }) {
   return (
-    <div className="gpt3__features-container-feature">
+    <div className="gpt3__features-container__feature">
       <div className="gpt3__features-container__feature-title">
         <div />
         <h1>{title}</h1>
-        <div className="gpt3__features-container_feature-text">{text}</div>
+      </div>
+      <div className="gpt3__features-container_feature-text">
+        <p>{text}</p>
       </div>
     </div>
   );
