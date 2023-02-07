0d3608898714245d6f14c56ecb512625887e258e
Steven Sidhu <steve.sidhu@hotmail.com>
2023-02-06 21:33:15 -0800

    importing and using props

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./feature.css";
 
-function Feature() {
-  return <div>Feature</div>;
+// passing title and text as props
+function Feature({ title, text }) {
+  return (
+    <div className="gpt3__features-container-feature">
+      <div className="gpt3__features-container__feature-title">
+        <div />
+        <h1>{title}</h1>
+        <div className="gpt3__features-container_feature-text">{text}</div>
+      </div>
+    </div>
+  );
 }
 
 export default Feature;
